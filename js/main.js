479017971101408e5a10d7b9b966ff542002e0b7
Alex Sirr <sirra@uw.edu>
2015-11-06 17:04:51 -0800

    day 2 work

--- js/main.js
+++ js/main.js
@@ -1,13 +1,81 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ['firebase']);
 var base = "https://api.spotify.com";
 
-var myCtrl = myApp.controller("myCtrl", function($scope, $http) {
+var myCtrl = myApp.controller("myCtrl", function($scope, $http, $firebaseAuth, $firebaseArray, $firebaseObject) {
+	// firebase actions
+	var ref = new Firebase("https://spotify-as-challenge.firebaseio.com/");
+    var userRef = ref.child("users");
+    var searchRef = ref.child("searches");
+
+    $scope.users = $firebaseObject(userRef);
+    $scope.searches = $firebaseArray(searchRef);
+
+    $scope.authObj = $firebaseAuth(ref);
+
+    var authData = $scope.authObj.$getAuth();
+    if (authData) {
+        $scope.userId = authData.uid;
+        document.getElementById("account-action").innerHTML = "Logout";
+    } 
+
+    $scope.signUp = function() {
+        // Create user
+        $scope.authObj.$createUser({
+            email: $scope.email,
+            password: $scope.password,          
+        })
+
+        // Once the user is created, call the logIn function
+        .then($scope.logIn)
+
+        // Once logged in, set and save the user data
+        .then(function(authData) {
+            $scope.userId = authData.uid;
+            $scope.users.$save()
+            $scope.closeSignin();
+            $scope.hideOverlay();
+            document.getElementById("account-action").innerHTML = "Logout";
+        })
+
+        // Catch any errors
+        .catch(function(error) {
+            console.error("Error: ", error);
+        });
+    }
+
+    // SignIn function
+    $scope.signIn = function() {
+        $scope.logIn().then(function(authData){
+            $scope.userId = authData.uid;
+            $scope.closeSignin();
+            $scope.hideOverlay();
+            document.getElementById("account-action").innerHTML = "Logout";
+        })
+    }
+
+    // LogIn function
+    $scope.logIn = function() {
+        return $scope.authObj.$authWithPassword({
+            email: $scope.email,
+            password: $scope.password
+        })
+    }
+
+    // LogOut function
+    $scope.logOut = function() {
+        $scope.authObj.$unauth()
+        $scope.userId = false
+        document.getElementById("account-action").innerHTML = "Sign In";
+    }
+
+    // Spotify actions
 	$scope.maxSize = 10;
 	$scope.reverseSort = true;
 	$scope.artist;
 
     $scope.execute = function() {
     	$scope.finished = false;
+    	console.log($scope.artist)
     	$scope.myArtists;
     	// playlist of every track from every related artist
 		$scope.completePlaylist = [];
@@ -64,8 +132,59 @@ var myCtrl = myApp.controller("myCtrl", function($scope, $http) {
   	$scope.changeInput = function(item) {
   		$scope.artist = item;
   	}
+
+  	$scope.hideOverlay = function() {
+  		document.getElementById("overlay").style.display = 'none';
+  		document.getElementById("signin").style.display = 'none';
+  		$scope.email = "";
+  		$scope.password = "";
+  	}
+
+  	// Button functions
+
+  	$scope.openLogin = function() {
+  		if (document.getElementById("account-action").innerHTML == "Logout") {
+  			$scope.logOut();
+  		} else {
+  			document.getElementById("signin").style.display = 'block';
+	  		document.getElementById("selection").style.display = 'block';
+	  		document.getElementById("login").style.display = 'none';
+	  		document.getElementById("register").style.display = 'none';
+	  		document.getElementById("signup-title").innerHTML = "Login";
+	  		document.getElementById("overlay").style.display = 'block';
+	  		$scope.email = "";
+	  		$scope.password = "";
+  		}
+  		
+  	}
+
+  	$scope.loginSelect = function() {
+  		document.getElementById("selection").style.display = 'none';
+  		document.getElementById("login").style.display = 'block';
+  		document.getElementById("signup-title").innerHTML = "Login";
+  	}
+
+  	$scope.registerSelect = function() {
+  		document.getElementById("selection").style.display = 'none';
+  		document.getElementById("register").style.display = 'block';
+  		document.getElementById("signup-title").innerHTML = "Register";
+  	}
+
+  	$scope.backSelect = function() {
+  		document.getElementById("selection").style.display = 'block';
+  		document.getElementById("login").style.display = 'none';
+  		document.getElementById("register").style.display = 'none';
+  		document.getElementById("signup-title").innerHTML = "Login";
+  		$scope.email = "";
+  		$scope.password = "";
+  	}
+
+  	$scope.closeSignin = function() {
+  		document.getElementById("overlay").style.display = 'none';
+  		document.getElementById("signin").style.display = 'none';
+  		$scope.email = "";
+  		$scope.password = "";
+  	}
+
 });
 
-$("#select-login").on("click", function() {
-	console.log(test);
-})
